f9f75ba41c574041fc99f89674bdd25921315c1d
Thibaut Sardan <github@thib.top>
2022-03-27 19:03:14 +0200

    still wasm not available

--- App.tsx
+++ App.tsx
@@ -1,25 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
+import '@polkadot/wasm-crypto/initOnlyAsm'
+import { waitReady, bip39Generate } from '@polkadot/wasm-crypto';
 
-import useCachedResources from './hooks/useCachedResources';
-import useColorScheme from './hooks/useColorScheme';
-import Navigation from './navigation';
-import { initWasm } from '@polkadot/wasm-crypto/initOnlyAsm'
+import { View, Text } from 'react-native'
 
-initWasm().catch(console.error);
 
 export default function App() {
-  const isLoadingComplete = useCachedResources();
-  const colorScheme = useColorScheme();
+  waitReady().then(() => console.log(bip39Generate(12))).catch(console.error);
 
-  if (!isLoadingComplete) {
-    return null;
-  } else {
-    return (
-      <SafeAreaProvider>
-        <Navigation colorScheme={colorScheme} />
-        <StatusBar />
-      </SafeAreaProvider>
-    );
-  }
+  return (
+    <View>
+      <Text>Hello</Text>
+    </View>
+  );
 }
--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -3,16 +3,12 @@ import { StyleSheet } from 'react-native';
 import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
 import { RootTabScreenProps } from '../types';
-import Identicon from '@polkadot/reactnative-identicon';
 
 export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'>) {
 
   return (
     <View style={styles.container}>
-      <Identicon
-        value="5GrwvaEF5zXb26Fz9rcQpDWS57CtERHpNehXCPcNoHGKutQY"
-        size={48}
-      />
+
       <Text style={styles.title}>Tab One</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
       <EditScreenInfo path="/screens/TabOneScreen.tsx" />
