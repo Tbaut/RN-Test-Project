bf37b5d12f75c6577dd7c6ceba20e5d67f77c7eb
Thibaut Sardan <github@thib.top>
2022-03-29 01:05:11 +0200

    working

--- App.tsx
+++ App.tsx
@@ -1,17 +1,14 @@
-import '@polkadot/wasm-crypto/initOnlyAsm'
-import { bip39Generate } from '@polkadot/wasm-crypto';
+import { bip39Generate, waitReady } from '@polkadot/wasm-crypto';
 
 import { View, Text } from 'react-native'
-import { cryptoWaitReady } from '@polkadot/util-crypto';
 import { useEffect } from 'react';
 
 
 export default function App() {
   useEffect((): void => {
-    // waitReady()
-    //   .then(() => setResult(bip39Generate(12)))
-    //   .catch(console.error);
-    cryptoWaitReady().then(() => console.log(bip39Generate(12))).catch(console.error);
+    waitReady()
+      .then(() => console.log(bip39Generate(12)))
+      .catch(console.error);
   }, []);
 
   return (
--- metro.config.js
+++ metro.config.js
@@ -6,6 +6,14 @@
  */
 module.exports = {
 	resolver: {
-		sourceExts: ['jsx', 'js', 'ts', 'tsx', 'cjs']
-	}
+		sourceExts: ['cjs', 'jsx', 'js', 'ts', 'tsx'],
+	},
+	transformer: {
+		getTransformOptions: async () => ({
+			transform: {
+				experimentalImportSupport: false,
+				inlineRequires: true,
+			},
+		}),
+	},
 };
