bd244f317799b34c9b4dac7e3093a8b09594c48d
Thibaut Sardan <github@thib.top>
2022-04-05 23:03:26 +0100

    reactnative-identicon

--- App.tsx
+++ App.tsx
@@ -1,19 +1,23 @@
-import { bip39Generate, waitReady } from '@polkadot/wasm-crypto';
-
 import { View, Text } from 'react-native'
 import { useEffect } from 'react';
-
+import { cryptoWaitReady, blake2AsHex } from '@polkadot/util-crypto';
+import Identicon from '@polkadot/reactnative-identicon';
 
 export default function App() {
   useEffect((): void => {
-    waitReady()
-      .then(() => console.log(bip39Generate(12)))
+    cryptoWaitReady()
+      .then(() => console.log(blake2AsHex('0x1234')))
       .catch(console.error);
   }, []);
 
   return (
     <View>
       <Text>Hello</Text>
+      <Identicon
+        value="5GrwvaEF5zXb26Fz9rcQpDWS57CtERHpNehXCPcNoHGKutQY"
+        size={48}
+      />
+      <Text>Hello 2</Text>
     </View>
   );
 }
